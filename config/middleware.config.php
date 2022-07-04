3abc7dbb39aa7b43d141373a37518aa806a3375c
Andrew Caya <andrewscaya@yahoo.ca>
2022-07-04 10:22:30 -0400

    2022-07-04 AC: Adds possible request query strings.

--- config/middleware.config.php
+++ config/middleware.config.php
@@ -1,6 +1,30 @@
 <?php
 
 $baseConfig['middleware'] = [
+    function ($request, $handler) {
+        if ((isset($request->getServerParams()['QUERY_STRING'])
+                && (strpos($request->getServerParams()['QUERY_STRING'], 'fbclid=') !== false
+                    || strpos($request->getServerParams()['QUERY_STRING'], 'ref=') !== false))
+            || (isset($request->getServerParams()['REDIRECT_QUERY_STRING'])
+                && (strpos($request->getServerParams()['REDIRECT_QUERY_STRING'], 'fbclid=') !== false
+                    || strpos($request->getServerParams()['REDIRECT_QUERY_STRING'], 'ref=') !== false))
+        ) {
+            $requestUriArray = explode('?', $request->getServerParams()['REQUEST_URI']);
+
+            if (empty($requestUriArray[0]) || $requestUriArray[0] === '/') {
+                $requestUri = '/index';
+            } else {
+                $requestUri = $requestUriArray[0];
+            }
+
+            $response = new \Laminas\Diactoros\Response();
+            $response = $response->withStatus('302');
+            $response = $response->withHeader('Location', $requestUri);
+            return $response;
+        }
+
+        return $handler->handle($request);
+    },
     /*'/foo' => function ($req, $handler) {
         $response = new \Laminas\Diactoros\Response();
         $response->getBody()->write('FOO!');
