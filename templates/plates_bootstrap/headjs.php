b2bd5b3aac02061687e210d3da229a4169b3d6b4
jtcuser <jtcuser@jtcdev.etista.com>
2022-11-21 16:56:04 -0500

    frontend-node-dev

--- templates/plates_bootstrap/headjs.php
+++ templates/plates_bootstrap/headjs.php
@@ -19,6 +19,10 @@
     <!-- CSS
     ================================================== -->
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" type='text/css'> 
+
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
     <!-- Fontawesome Icon font -->
