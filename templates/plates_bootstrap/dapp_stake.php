6ae2e477f6c8ccebcc17cdc07686f07ab6d162ac
jtcaya <jt.caya@etista.com>
2024-06-27 12:17:58 -0400

    2024-06-27 JTC: Makes some changes to the Staking UI.

--- templates/plates_bootstrap/dapp_stake.php
+++ templates/plates_bootstrap/dapp_stake.php
@@ -1,41 +1,208 @@
 <div class="top">
-    <div class="row" style="margin-bottom: 5px;">
-        <div class="col-sm-4" style="padding-left: 0; padding-right: 0;">
-            <button class="transfer" type="button" id="Transfer" onclick="getDappPage(4)">
-                <a class="wrap-unwrap-text">
-                    <span id="Transfer">Transfer</span>
-                </a>
-            </button>
-        </div>
-        <div class="col-sm-4" style="padding-left: 0; padding-right: 0;">
-            <button class="stake" type="button" id="Stake" onclick="getDappPage(4)">
-                <a class="wrap-unwrap-text">
-                    <span id="Stake">Stake</span>
-                </a>
-            </button>
-        </div>
-        <div class="col-sm-4" style="padding-left: 0; padding-right: 0;">
-            <button class="stakeRewards" type="button" id="StakeRewards" onclick="getDappPage(4)">
-                <a class="wrap-unwrap-text">
-                    <span id="StakeRewards">Rewards</span>
-                </a>
-            </button>
+<div class="wrap-box" style="text-align: center !important; padding: 16px 0 !important;">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 style="color: #383a3b; font-size: 23px">How would you like to access your Wallet?</h1>
         </div>
     </div>
 </div>
-<div class="row">
-    <div class="col-sm-12">
-        <button id="ContinueMetamask" class="connect-wallet" style="float: none; margin-left: auto; margin-right: auto;"><i class="connect-wallet-text" id="ConnectWalletText">Metamask</i></button>
-    </div>
 </div>
-<div class="row">
-    <div class="col-sm-12">
-        <button id="ContinueLedger" class="connect-wallet" style="float: none; margin-left: auto; margin-right: auto;"><i class="connect-wallet-text" id="ConnectWalletText">Ledger</i></button>
+<div class="row" style="padding: 0 20px;">
+    <div class="col-sm-6">
+        <button id="ContinueMetamask" class="continue-wallet" style="float: none; margin-left: auto; margin-right: auto;">
+            <div class="wallet-icon">
+                <svg
+                version="1.1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 204.8 192.4"
+                style="enable-background:new 0 0 204.8 192.4; width: 100%; height: 100%;"
+                xml:space="preserve"
+                id="svg29"
+                sodipodi:docname="SVG_MetaMask_Icon_Color.svg"
+                inkscape:version="1.3 (0e150ed, 2023-07-21)"
+                xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+                xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg"><defs
+                id="defs29">&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;		&#10;	</defs><sodipodi:namedview
+                id="namedview29"
+                pagecolor="#ffffff"
+                bordercolor="#666666"
+                borderopacity="1.0"
+                inkscape:showpageshadow="2"
+                inkscape:pageopacity="0.0"
+                inkscape:pagecheckerboard="0"
+                inkscape:deskcolor="#d1d1d1"
+                inkscape:zoom="1.40625"
+                inkscape:cx="76.08889"
+                inkscape:cy="140.8"
+                inkscape:window-width="1366"
+                inkscape:window-height="704"
+                inkscape:window-x="0"
+                inkscape:window-y="0"
+                inkscape:window-maximized="1"
+                inkscape:current-layer="svg29" />&#10;<style
+                type="text/css"
+                id="style1">&#10;	.st0{display:none;}&#10;	.st1{fill:#F5841F;}&#10;	.st2{fill:#E27625;}&#10;	.st3{fill:#D7C1B3;}&#10;	.st4{fill:#2F343B;}&#10;	.st5{fill:#CC6228;}&#10;	.st6{fill:#C0AD9E;}&#10;	.st7{fill:#763E1A;}&#10;</style>&#10;<g
+                id="bg_x28_do_not_export_x29_"
+                class="st0">&#10;</g>&#10;<g
+                id="MM_Head_background__x28_Do_not_edit_x29_">&#10;	&#10;</g>&#10;<g
+                id="Logos">&#10;	<polygon
+                class="st2"
+                points="122.7,43.7 171.5,25.6 111.6,69.7 "
+                id="polygon1"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="82,43.7 33.2,25.6 92.6,70.1 "
+                id="polygon2"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="168.2,161.5 178,128.4 150,127.9 134,152.1 "
+                id="polygon3"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="70.7,152.1 54.8,127.9 26.9,128.4 36.6,161.5 "
+                id="polygon4"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="68.9,86.9 59.4,101.2 93.2,102.7 92.1,66.5 "
+                id="polygon5"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="111.6,102.7 145.4,101.2 135.9,86.9 112.3,66.1 "
+                id="polygon6"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="73.5,128.7 70.7,152.1 91.2,142.3 "
+                id="polygon7"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="131.2,128.7 113.6,142.3 134,152.1 "
+                id="polygon8"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st3"
+                points="115.3,155.5 115.1,161.1 134,152.1 113.6,142.3 "
+                id="polygon9"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st3"
+                points="89.6,155.5 91.2,142.3 70.7,152.1 89.7,161.1 "
+                id="polygon10"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st5"
+                points="54.8,128.4 70.7,152.1 73.7,127.9 "
+                id="polygon13"
+                style="fill:#ffffff;fill-opacity:0.30000001" /><polygon
+                class="st5"
+                points="150,128.4 131.1,127.9 134,152.1 "
+                id="polygon14"
+                style="fill:#ffffff;fill-opacity:0.30000001" /><polygon
+                class="st5"
+                points="114.7,119.9 119.7,109.5 131.7,115 145.4,101.2 111.6,102.7 "
+                id="polygon15"
+                style="fill:#ffffff;fill-opacity:0.30000001" /><polygon
+                class="st5"
+                points="90,119.9 93.2,102.7 59.4,101.2 73.1,115 85.1,109.5 "
+                id="polygon16"
+                style="fill:#ffffff;fill-opacity:0.30000001" /><polygon
+                class="st2"
+                points="73.1,115 59.4,101.2 73.5,128.7 "
+                id="polygon17"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="145.4,101.2 131.7,115 131.2,128.7 "
+                id="polygon18"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="94,140.3 94.9,113.5 93.2,102.7 90,119.9 "
+                id="polygon19"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st2"
+                points="110.7,140.3 114.7,119.9 111.6,102.7 109.9,113.4 "
+                id="polygon20"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st1"
+                points="113.6,142.3 131.2,128.7 131.7,115 114.7,119.9 110.7,140.3 "
+                id="polygon21"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st1"
+                points="91.2,142.3 94,140.3 90,119.9 73.1,115 73.5,128.7 "
+                id="polygon22"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st6"
+                points="113.7,154.2 91,154.2 89.6,155.5 89.7,161.1 70.7,152.1 77.3,157.5 90.8,166.8 113.9,166.8 127.4,157.5 134,152.1 115.1,161.1 115.3,155.5 "
+                id="polygon23"
+                style="fill:#ffffff;fill-opacity:0.60000002" /><polygon
+                class="st7"
+                points="171.5,25.6 113.6,68.2 135.9,86.9 167.4,96.1 174.3,88 171.3,85.8 176.1,81.5 172.4,78.6 177.2,75 174.1,72.6 179.1,48.2 "
+                id="polygon25"
+                style="fill:#ffffff;fill-opacity:0.2" /><polygon
+                class="st7"
+                points="27.5,75 32.3,78.7 28.6,81.5 33.4,85.8 30.4,88 37.4,96.1 68.9,86.9 91.2,68.2 33.2,25.6 25.6,48.2 30.7,72.6 "
+                id="polygon26"
+                style="fill:#ffffff;fill-opacity:0.2" /><polygon
+                class="st1"
+                points="145.4,101.2 131.2,128.7 150,128.4 178,128.4 167.4,96.1 135.9,86.9 "
+                id="polygon27"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st1"
+                points="26.9,128.4 54.8,128.4 73.5,128.7 59.4,101.2 68.9,86.9 37.4,96.1 "
+                id="polygon28"
+                style="fill:#ffffff;fill-opacity:1" /><polygon
+                class="st1"
+                points="122.7,43.7 82,43.7 91.2,68.2 93.2,102.7 94,113.5 94,140.3 110.7,140.3 110.8,113.5 111.6,102.7 113.6,68.2 "
+                id="polygon29"
+                style="fill:#ffffff;fill-opacity:1" />&#10;</g>&#10;</svg>
+            </div>
+            <i class="connect-wallet-text" id="ConnectWalletText">Metamask</i>
+        </button>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-        <span> How would you like to access your Wallet?</span>
+    <div class="col-sm-6">
+        <button id="ContinueLedger" class="continue-wallet" style="float: none; margin-left: auto; margin-right: auto;">
+            <div class="wallet-icon">
+                <svg
+                width="147"
+                height="128"
+                viewBox="0 0 204 192"
+                style="width: 100%; height: 100%;"
+                fill="none"
+                version="1.1"
+                id="svg1"
+                sodipodi:docname="SVG_Ledger_Icon.svg"
+                inkscape:version="1.3 (0e150ed, 2023-07-21)"
+                xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+                xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg">
+                <defs
+                    id="defs1" />
+                <sodipodi:namedview
+                    id="namedview1"
+                    pagecolor="#ffffff"
+                    bordercolor="#666666"
+                    borderopacity="1.0"
+                    inkscape:showpageshadow="2"
+                    inkscape:pageopacity="0.0"
+                    inkscape:pagecheckerboard="0"
+                    inkscape:deskcolor="#d1d1d1"
+                    inkscape:zoom="1.1352191"
+                    inkscape:cx="18.058189"
+                    inkscape:cy="139.18019"
+                    inkscape:window-width="1366"
+                    inkscape:window-height="704"
+                    inkscape:window-x="0"
+                    inkscape:window-y="0"
+                    inkscape:window-maximized="1"
+                    inkscape:current-layer="svg1" />
+                <path
+                    d="M 28.5,123.6548 V 160 h 55.293 v -8.06 H 36.55631 v -28.2852 z m 138.944,0 V 151.94 h -47.237 v 8.058 H 175.5 V 123.6548 Z M 83.8733,68.3452 v 55.3077 h 36.3337 v -7.2687 H 91.9296 V 68.3452 Z M 28.5,32 v 36.3452 h 8.05631 V 40.05844 H 83.793 V 32 Z m 91.707,0 v 8.05844 h 47.237 V 68.3452 H 175.5 V 32 Z"
+                    fill="#000000"
+                    id="path1"
+                    style="fill:#ffffff;fill-opacity:1" />
+                </svg>
+            </div>
+            <i class="connect-wallet-text" id="ConnectWalletText">Ledger</i>
+        </button>
     </div>
 </div>
 <div class="row">
