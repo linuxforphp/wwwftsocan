43fd29dd6ba9fcf8ec4a449b825c09304a3cc6ad
jtcaya <jt.caya@etista.com>
2024-08-12 19:46:26 -0400

    2024-08-12 JTC: Adds Current Wallet icon.

--- templates/plates_bootstrap/dapp_index.php
+++ templates/plates_bootstrap/dapp_index.php
@@ -17,6 +17,33 @@
         <?=$this->section('navbar_dapp', $this->fetch('navbar_dapp', ['view' => $view]))?>
         <div id="Accounts" data-address=""></div>
         <div class="dapp-container" id="dapp-root"></div>
+        <div id="currentWallet" class="current-wallet">
+            <div class="current-wallet-icon">
+                <div class="current-wallet-icon-border"></div>
+            </div>
+            <svg
+            class="btn-bell"
+            viewBox="0 0 1503 1504"
+            fill="none"
+            version="1.1"
+            id="svg1"
+            sodipodi:docname="avalanche-avax-logo.svg"
+            inkscape:version="1.3 (0e150ed, 2023-07-21)"
+            xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+            xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:svg="http://www.w3.org/2000/svg">
+            <defs
+                id="defs1" />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M 538.688,1050.86 H 392.94 c -30.626,0 -45.754,0 -54.978,-5.9 -9.963,-6.46 -16.051,-17.16 -16.789,-28.97 -0.554,-10.88 7.011,-24.168 22.139,-50.735 l 359.87,-634.32 c 15.313,-26.936 23.061,-40.404 32.839,-45.385 10.516,-5.35 23.062,-5.35 33.578,0 9.778,4.981 17.527,18.449 32.839,45.385 l 73.982,129.144 0.377,0.659 c 16.539,28.897 24.926,43.551 28.588,58.931 4.058,16.789 4.058,34.5 0,51.289 -3.69,15.497 -11.992,30.257 -28.781,59.591 l -189.031,334.153 -0.489,0.856 c -16.648,29.135 -25.085,43.902 -36.778,55.042 -12.73,12.18 -28.043,21.03 -44.832,26.02 -15.313,4.24 -32.47,4.24 -66.786,4.24 z m 368.062,0 h 208.84 c 30.81,0 46.31,0 55.54,-6.08 9.96,-6.46 16.23,-17.35 16.79,-29.15 0.53,-10.53 -6.87,-23.3 -21.37,-48.323 -0.5,-0.852 -1,-1.719 -1.51,-2.601 l -104.61,-178.956 -1.19,-2.015 c -14.7,-24.858 -22.12,-37.411 -31.65,-42.263 -10.51,-5.351 -22.88,-5.351 -33.391,0 -9.594,4.981 -17.342,18.08 -32.655,44.462 l -104.238,178.957 -0.357,0.616 c -15.259,26.34 -22.885,39.503 -22.335,50.303 0.738,11.81 6.826,22.69 16.788,29.15 9.041,5.9 24.538,5.9 55.348,5.9 z"
+                fill="#FFFFFF"
+                id="path1"
+                sodipodi:nodetypes="ssccccccccsccccsccsssccccccscccsccs" />
+            </svg>
+        </div>
     </main>
 <?php else: ?>
     <?=$this->section('navbar', $this->fetch('navbar', ['view' => $view]))?>
