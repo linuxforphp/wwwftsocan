2d385acb2d90123d8087490973fd69af4858819e
jtcaya <jt.caya@etista.com>
2024-09-03 14:16:57 -0400

    2024-09-03 JTC: Fixes some WalletConnect connection issues.

--- src/dapp-common.js
+++ src/dapp-common.js
@@ -881,7 +881,7 @@ async function createSelectedNetwork(DappObject) {
                                 '--wcm-overlay-background-color': "rgba(0, 0, 0, 0.5)",
                             },
                         },
-                        chains: [14, 19],
+                        optionalChains: [14, 19],
     
                         /*Optional - Add custom RPCs for each supported chain*/
                         rpcMap: {
@@ -890,7 +890,14 @@ async function createSelectedNetwork(DappObject) {
                         }
                     });
 
-                    await DappObject.walletConnectEVMProvider.connect();
+                    try {
+                        await DappObject.walletConnectEVMProvider.connect();
+                    } catch (error) {
+                        networkSelectBox.options[0].setAttribute('selected', 'selected');
+                        networkSelectBox.options[1].removeAttribute('selected');
+                        networkSelectBox.options.selectedIndex = 0;
+                        resolve();
+                    }
                 }
 
                 var chainIdHexPromise = await DappObject.walletConnectEVMProvider.request({method: 'eth_chainId'}).then(async function(chainIdHex) {
@@ -1225,6 +1232,54 @@ async function ConnectWalletClick(rpcUrl, flrAddr, DappObject, pageIndex, Handle
             DappObject.isAccountConnected = true;
 
         } else if (DappObject.walletIndex === 2 && (typeof PassedPublicKey === "undefined" || PassedPublicKey === "")) {
+            if (DappObject.walletConnectEVMProvider === undefined) {
+                DappObject.walletConnectEVMProvider = await walletConnectProvider.init({
+                    projectId: '89353513e21086611c5118bd063aae5b',
+                    metadata: {
+                        name: 'FTSOCAN DApp',
+                        description: "The FTSOCAN DApp allows you to manage your $FLR and $SGB tokens in a secure, lightweight, and intuitive way. Wrap, delegate and claim your token rewards, using the DApp's fully responsive, and mobile-friendly interface.",
+                        url: 'https://ftsocan.com/dapp/index', // origin must match your domain & subdomain
+                        icons: ['https://avatars.githubusercontent.com/u/37784886']
+                    },
+                    showQrModal: true,
+                    qrModalOptions: {
+                        themeMode: "light",
+                        explorerRecommendedWalletIds: [
+                            // Bifrost Wallet
+                            "37a686ab6223cd42e2886ed6e5477fce100a4fb565dcd57ed4f81f7c12e93053",
+                            // Metamask
+                            "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+                            // Web3AUTH
+                            "78aaedfb74f2f4737134f2aaa78871f15ff0a2828ecb0ddc5b068a1f57bb4213",
+                            // Ledger
+                            '19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927',
+                        ],
+                        explorerExcludedWalletIds: [
+                            // Ledger
+                            //'19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927'
+                        ],
+                        themeVariables: {
+                            '--wcm-z-index': 9998,
+                            '--wcm-font-family': "'Poppins', sans-serif",
+                            '--wcm-accent-color': "rgba(255, 49, 32, 0.8)",
+                            '--wcm-background-color': "rgba(255, 49, 32, 0.8)",
+                            '--wcm-overlay-background-color': "rgba(0, 0, 0, 0.5)",
+                        },
+                    },
+                    optionalChains: [14, 19],
+
+                    /*Optional - Add custom RPCs for each supported chain*/
+                    rpcMap: {
+                        14: 'https://sbi.flr.ftsocan.com',
+                        19: 'https://sbi.sgb.ftsocan.com'
+                    }
+                });
+            }
+
+            if (DappObject.isAccountConnected === false) {
+                await DappObject.walletConnectEVMProvider.connect();
+            }
+
             const accounts = await DappObject.walletConnectEVMProvider.request({method: 'eth_requestAccounts'});
             
             account = accounts[0];
@@ -2134,6 +2189,54 @@ async function ConnectPChainClickStake(DappObject, HandleClick, PassedPublicKey,
 
             flrPublicKey = await GetPublicKey(account, message, DappObject.signatureStaking);
         }  else if (DappObject.walletIndex === 2 && typeof PassedPublicKey === "undefined") {
+            if (DappObject.walletConnectEVMProvider === undefined) {
+                DappObject.walletConnectEVMProvider = await walletConnectProvider.init({
+                    projectId: '89353513e21086611c5118bd063aae5b',
+                    metadata: {
+                        name: 'FTSOCAN DApp',
+                        description: "The FTSOCAN DApp allows you to manage your $FLR and $SGB tokens in a secure, lightweight, and intuitive way. Wrap, delegate and claim your token rewards, using the DApp's fully responsive, and mobile-friendly interface.",
+                        url: 'https://ftsocan.com/dapp/index', // origin must match your domain & subdomain
+                        icons: ['https://avatars.githubusercontent.com/u/37784886']
+                    },
+                    showQrModal: true,
+                    qrModalOptions: {
+                        themeMode: "light",
+                        explorerRecommendedWalletIds: [
+                            // Bifrost Wallet
+                            "37a686ab6223cd42e2886ed6e5477fce100a4fb565dcd57ed4f81f7c12e93053",
+                            // Metamask
+                            "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+                            // Web3AUTH
+                            "78aaedfb74f2f4737134f2aaa78871f15ff0a2828ecb0ddc5b068a1f57bb4213",
+                            // Ledger
+                            '19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927',
+                        ],
+                        explorerExcludedWalletIds: [
+                            // Ledger
+                            //'19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927'
+                        ],
+                        themeVariables: {
+                            '--wcm-z-index': 9998,
+                            '--wcm-font-family': "'Poppins', sans-serif",
+                            '--wcm-accent-color': "rgba(255, 49, 32, 0.8)",
+                            '--wcm-background-color': "rgba(255, 49, 32, 0.8)",
+                            '--wcm-overlay-background-color': "rgba(0, 0, 0, 0.5)",
+                        },
+                    },
+                    optionalChains: [14, 19],
+
+                    /*Optional - Add custom RPCs for each supported chain*/
+                    rpcMap: {
+                        14: 'https://sbi.flr.ftsocan.com',
+                        19: 'https://sbi.sgb.ftsocan.com'
+                    }
+                });
+            }
+            
+            if (DappObject.isAccountConnected === false) {
+                await DappObject.walletConnectEVMProvider.connect();
+            }
+            
             const accounts = await DappObject.walletConnectEVMProvider.request({method: 'eth_requestAccounts'});
             
             account = accounts[0];
@@ -4003,6 +4106,10 @@ window.dappInit = async (option, stakingOption) => {
                         handleChainChanged(DappObject);
                     });
 
+                    DappObject.walletConnectEVMProvider.on("connect", async () => {
+                        DappObject.isAccountConnected = true;
+                    });
+
                     DappObject.walletConnectEVMProvider.on("disconnect", async () => {
                         getDappPage(4);
                     });
@@ -4147,6 +4254,10 @@ window.dappInit = async (option, stakingOption) => {
 
                     DappObject.walletConnectEVMProvider.on("chainChanged", async () => {
                         handleChainChanged(DappObject);
+                    });
+
+                    DappObject.walletConnectEVMProvider.on("connect", async () => {
+                        DappObject.isAccountConnected = true;
                     });
 
                     DappObject.walletConnectEVMProvider.on("disconnect", async () => {
@@ -4640,6 +4751,10 @@ window.dappInit = async (option, stakingOption) => {
                         handleChainChanged(DappObject);
                     });
 
+                    DappObject.walletConnectEVMProvider.on("connect", async () => {
+                        DappObject.isAccountConnected = true;
+                    });
+
                     DappObject.walletConnectEVMProvider.on("disconnect", async () => {
                         getDappPage(4);
                     });
@@ -4859,6 +4974,10 @@ window.dappInit = async (option, stakingOption) => {
 
             DappObject.walletConnectEVMProvider.on("chainChanged", async () => {
                 handleChainChangedStake(DappObject);
+            });
+
+            DappObject.walletConnectEVMProvider.on("connect", async () => {
+                DappObject.isAccountConnected = true;
             });
 
             DappObject.walletConnectEVMProvider.on("disconnect", async () => {
