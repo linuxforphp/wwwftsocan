4ade12f849af5942c18ea27a634824e5be78897c
jtcaya <jt.caya@etista.com>
2026-01-05 14:44:13 -0500

    2026-01-05 JTC: Revamps the index page.

--- templates/plates_bootstrap/index_index.php
+++ templates/plates_bootstrap/index_index.php
@@ -25,8 +25,10 @@
                     <img src="<?=$view['urlbaseaddr']?>img/logo-dark-2.svg"/>
                     <div class="h1">
                         FTSO Canada<br />
-                        <span class="lead"><?=_("index_lead")?></span>
                     </div>
+                    <span class="lead"><?=_("index_lead")?></span>
+                    <br />
+                    <button style="background-color: rgba(253, 0, 15, 0.9);" onclick="getDocsPageNewTab(1, '<?=$view['urlbaseaddr']?>dapp/index')" class="connect-wallet hero-btn"><i class="connect-wallet-text" style="font-weight: 500;">Open App</i></button>
                 </div>
             </div>
         </div>
@@ -87,7 +89,7 @@
 
                     <div class="sec-title text-center dark wow animated fadeInDown">
                         <h2><?=_("index_followus")?></h2>
-                        <p><?=_("index_invitation")?></p>
+                        <p style="padding: 0 20px;"><?=_("index_invitation")?></p>
                     </div>
 
                     <ul class="social-button">
